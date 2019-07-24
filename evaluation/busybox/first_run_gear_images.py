fd002c71716b4ece6146e32b7ee19da158ec2922
Bian Shengwei <bianshengwei@gmail.com>
2019-07-24 21:01:46 +0800

    add busybox

---
+++ evaluation/busybox/first_run_gear_images.py
@@ -0,0 +1,168 @@
+import sys
+# package need to be installed, pip install docker
+import docker 
+import time
+import yaml
+import os
+import random
+import subprocess
+import signal
+import urllib2
+import psycopg2
+import shutil
+import pymongo
+
+auto = False
+
+private_registry = "202.114.10.146:9999/"
+suffix = "-gear"
+
+apppath = ""
+
+# run paraments
+hostPort = 8080
+localVolume = "/var/lib/gear/volume"
+pwd = os.getcwd()
+
+runEnvironment = []
+runPorts = {}
+runVolumes = {}
+runWorking_dir = ""
+runCommand = "echo hello"
+waitline = "hello"
+
+
+
+class Runner:
+
+    def __init__(self, images):  
+        self.images_to_pull = images
+
+    def check(self):
+        # detect whether the file exists, if true, delete it
+        if os.path.exists("./images_run.txt"):
+            os.remove("./images_run.txt")
+    
+    def run(self):
+        self.check()
+
+        client = docker.from_env()
+        # if don't give a tag, then all image under this registry will be pulled
+        repos = self.images_to_pull[0]["repo"]
+
+        for repo in repos:
+            tags = self.images_to_pull[1][repo]
+            for tag in tags:
+                private_repo = private_registry + repo + suffix + ":" + tag
+
+                if os.path.exists(localVolume) == False:
+                    os.makedirs(localVolume)
+
+                print "start running: ", private_repo
+
+                # create a random name
+                runName = '%d' % (random.randint(1,100000000))
+
+                # get present time
+                startTime = time.time()
+
+                # get present net data
+                cnetdata = get_net_data()
+
+                # run images
+                container = client.containers.create(image=private_repo, environment=runEnvironment,
+                                    ports=runPorts, volumes=runVolumes, working_dir=runWorking_dir,
+                                    command=runCommand, name=runName, detach=True)
+
+                container.start()
+
+                while True:
+                    if container.logs().find(waitline) >= 0:
+                        break
+                    else:
+                        time.sleep(0.01)
+                        pass
+
+                while True:
+                    if time.time() - startTime > 600:
+                        break
+
+                    try:
+                        container.logs().find(runWaitLine) >= 0
+                        break
+                    except:
+                        time.sleep(0.01) # wait 10ms
+                        pass
+
+                # print run time
+                finishTime = time.time() - startTime
+
+                print "finished in " , finishTime, "s"
+
+                print "pull data: ", get_net_data() - cnetdata
+
+                print "\n"
+
+                try: 
+                    container.kill()
+                except:
+                    print "kill fail!"
+                    pass
+
+                container.remove(force=True)
+                # cmd = '%s kill %s' % ("docker", runName)
+                # rc = os.system(cmd)
+                # assert(rc == 0)
+
+                # record the image and its Running time
+                self.record(private_repo, tag, finishTime)
+
+                if auto != True: 
+                    raw_input("Next?")
+                else:
+                    time.sleep(5)
+
+                shutil.rmtree(localVolume)
+
+    def record(self, repo, tag, time):
+        with open("./images_run.txt", "a") as f:
+            f.write("repo: "+str(repo)+" tag: "+str(tag)+" time: "+str(time)+"\n")
+
+class Generator:
+    
+    def __init__(self, profilePath=""):
+        self.profilePath = profilePath
+    
+    def generateFromProfile(self):
+        if self.profilePath == "":
+            print "Error: profile path is null"
+        
+        with open(self.profilePath, 'r') as f:
+            self.images = yaml.load(f)
+
+        return self.images
+
+def get_net_data():
+    netCard = "/proc/net/dev"
+    fd = open(netCard, "r")
+
+    for line in fd.readlines():
+        if line.find("enp0s3") >= 0:
+            field = line.split()
+            data = float(field[1]) / 1024.0 / 1024.0
+
+    fd.close()
+    return data
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        auto = True
+
+    generator = Generator(os.path.split(os.path.realpath(__file__))[0]+"/image_versions.yaml")
+
+    images = generator.generateFromProfile()
+
+    runner = Runner(images)
+
+    runner.run()
---
+++ evaluation/busybox/second_pull_gear_images_from_private_registry.py
@@ -0,0 +1,110 @@
+import sys
+# package need to be installed, pip install docker
+import docker 
+import time
+import yaml
+import os
+
+auto = False
+
+private_registry = "202.114.10.146:9999/"
+suffix = "-gearmd"
+
+class Puller:
+
+    def __init__(self, images):  
+        self.images_to_pull = images
+
+    def check(self):
+        # detect whether the file exists, if true, delete it
+        if os.path.exists("./images_pulled.txt"):
+            os.remove("./images_pulled.txt")
+    
+    def pull(self):
+        self.check()
+
+        client = docker.from_env()
+        # if don't give a tag, then all image under this registry will be pulled
+        repos = self.images_to_pull[0]["repo"]
+
+        for repo in repos:
+            tags = self.images_to_pull[1][repo]
+            for tag in tags:
+                print "start pulling: ", private_registry+repo+suffix, ":", tag
+
+                # get present time
+                startTime = time.time()
+
+                # get present net data
+                cnetdata = get_net_data()
+
+                # pull images
+                try:
+                    image_pulled = client.images.pull(repository=private_registry+repo+suffix, tag=str(tag))
+
+                    # print pull time
+                    finishTime = time.time() - startTime
+
+                    print "finished in " , finishTime, "s"
+
+                    # get image's size
+                    size = image_pulled.attrs[u'Size'] / 1000000.0
+                    print "image size: ", size
+
+                    print "pull data: ", get_net_data() - cnetdata
+
+                    print "\n"
+
+                    # record the image and its pulling time
+                    self.record(private_registry+repo+suffix, tag, finishTime, size)
+
+                except docker.errors.NotFound:
+                    print private_registry+repo+suffix + " not found...\n\n"
+                except docker.errors.ImageNotFound:
+                    print private_registry+repo+suffix + " image not fount...\n\n"
+
+                if auto != True: 
+                    raw_input("Next?")
+
+    def record(self, repo, tag, time, size):
+        with open("./images_pulled.txt", "a") as f:
+            f.write("repo: "+str(repo)+" tag: "+str(tag)+" time: "+str(time)+" size: "+str(size)+"\n")
+
+class Generator:
+    
+    def __init__(self, profilePath=""):
+        self.profilePath = profilePath
+    
+    def generateFromProfile(self):
+        if self.profilePath == "":
+            print "Error: profile path is null"
+        
+        with open(self.profilePath, 'r') as f:
+            self.images = yaml.load(f)
+
+        return self.images
+
+def get_net_data():
+    netCard = "/proc/net/dev"
+    fd = open(netCard, "r")
+
+    for line in fd.readlines():
+        if line.find("enp0s3") >= 0:
+            field = line.split()
+            data = float(field[1]) / 1024.0 / 1024.0
+
+    fd.close()
+    return data
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        auto = True
+
+    generator = Generator(os.path.split(os.path.realpath(__file__))[0]+"/image_versions.yaml")
+
+    images = generator.generateFromProfile()
+
+    puller = Puller(images)
+
+    puller.pull()
