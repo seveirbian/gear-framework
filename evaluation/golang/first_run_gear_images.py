33036bf03b80b2abfbf662209d866c595225a9c6
Bian Shengwei <bianshengwei@gmail.com>
2019-08-30 15:58:20 +0800

    update golang

--- evaluation/golang/first_run_gear_images.py
+++ evaluation/golang/first_run_gear_images.py
@@ -8,9 +8,7 @@ import random
 import subprocess
 import signal
 import urllib2
-import psycopg2
 import shutil
-import pymongo
 import xlwt
 # package need to be installed, apt-get install python-mysqldb
 import MySQLdb
@@ -25,7 +23,7 @@ apppath = ""
 # run paraments
 hostPort = 5000
 localVolume = ""
-pwd = os.getcwd()
+pwd = os.path.split(os.path.realpath(__file__))[0]
 
 runEnvironment = []
 runPorts = {}
@@ -35,7 +33,7 @@ runCommand = "go run /hello.go"
 waitline = "hello"
 
 # result
-result = [["tag", "finishTime", "data"], ]
+result = [["tag", "finishTime", "local data", "pull data"], ]
 
 class Runner:
 
@@ -87,17 +85,22 @@ class Runner:
                     elif container.logs().find(waitline) >= 0:
                         break
                     else:
-                        time.sleep(0.01)
+                        time.sleep(0.1)
                         pass
-
+                        
                 # print run time
                 finishTime = time.time() - startTime
 
                 print "finished in " , finishTime, "s"
 
-                data = get_net_data() - cnetdata
+                container_path = os.path.join("/var/lib/gear/private", private_repo)
+                local_data = subprocess.check_output(['du','-sh', container_path]).split()[0].decode('utf-8')
 
-                print "pull data: ", data
+                print "local data: ", local_data
+
+                pull_data = get_net_data() - cnetdata
+
+                print "pull data: ", pull_data
 
                 print "\n"
 
@@ -113,7 +116,7 @@ class Runner:
                 # assert(rc == 0)
 
                 # record the image and its Running time
-                result.append([tag, finishTime, data])
+                result.append([tag, finishTime, local_data, pull_data])
 
                 if auto != True: 
                     raw_input("Next?")
@@ -170,4 +173,4 @@ if __name__ == "__main__":
         for column in range(len(result[row])):
             sheet.write(row, column, result[row][column])
 
-    workbook.save("./first_run.xls")
+    workbook.save(os.path.split(os.path.realpath(__file__))[0]+"/first_run.xls")
--- evaluation/golang/first_run_gear_images_without_cache.py
+++ evaluation/golang/first_run_gear_images_without_cache.py
@@ -7,10 +7,8 @@ import os
 import random
 import subprocess
 import signal
-import shutil
 import urllib2
-import psycopg2
-import pymongo
+import shutil
 import xlwt
 # package need to be installed, apt-get install python-mysqldb
 import MySQLdb
@@ -25,7 +23,7 @@ apppath = ""
 # run paraments
 hostPort = 5000
 localVolume = ""
-pwd = os.getcwd()
+pwd = os.path.split(os.path.realpath(__file__))[0]
 
 runEnvironment = []
 runPorts = {}
@@ -35,7 +33,7 @@ runCommand = "go run /hello.go"
 waitline = "hello"
 
 # result
-result = [["tag", "finishTime", "data", "file_num"], ]
+result = [["tag", "finishTime", "local data", "pull data", "file_num"], ]
 
 class Runner:
 
@@ -87,7 +85,7 @@ class Runner:
                     elif container.logs().find(waitline) >= 0:
                         break
                     else:
-                        time.sleep(0.01)
+                        time.sleep(0.1)
                         pass
 
                 # print run time
@@ -95,9 +93,14 @@ class Runner:
 
                 print "finished in " , finishTime, "s"
 
-                data = get_net_data() - cnetdata
+                container_path = os.path.join("/var/lib/gear/private", private_repo)
+                local_data = subprocess.check_output(['du','-sh', container_path]).split()[0].decode('utf-8')
 
-                print "pull data: ", data
+                print "local data: ", local_data
+
+                pull_data = get_net_data() - cnetdata
+
+                print "pull data: ", pull_data
 
                 try: 
                     container.kill()
@@ -125,7 +128,7 @@ class Runner:
                 print "empty cache! \n"
 
                 # record the image and its Running time
-                result.append([tag, finishTime, data, file_num])
+                result.append([tag, finishTime, local_data, pull_data, file_num])
 
                 if auto != True: 
                     raw_input("Next?")
@@ -182,4 +185,4 @@ if __name__ == "__main__":
         for column in range(len(result[row])):
             sheet.write(row, column, result[row][column])
 
-    workbook.save("./first_run_without_cache.xls")
+    workbook.save(os.path.split(os.path.realpath(__file__))[0]+"/first_run_without_cache.xls")
