1b5d38811d8adb297a50589bbf6002230e4c0f8a
Bian Shengwei <bianshengwei@gmail.com>
2019-09-01 00:00:57 +0800

    update alpine

--- evaluation/alpine/run_docker_images.py
+++ evaluation/alpine/run_docker_images.py
@@ -8,8 +8,6 @@ import random
 import subprocess
 import signal
 import urllib2
-# package need to be installed, apt-get install python-pymongo
-import pymongo
 import shutil
 import xlwt
 # package need to be installed, apt-get install python-mysqldb
@@ -24,7 +22,7 @@ apppath = ""
 # run paraments
 hostPort = 5000
 localVolume = ""
-pwd = os.getcwd()
+pwd = os.path.split(os.path.realpath(__file__))[0]
 
 runEnvironment = []
 runPorts = {}
@@ -83,7 +81,7 @@ class Runner:
                     elif container.logs().find(waitline) >= 0:
                         break
                     else:
-                        time.sleep(0.01)
+                        time.sleep(0.1)
                         pass
 
                 # print run time
@@ -158,4 +156,4 @@ if __name__ == "__main__":
         for column in range(len(result[row])):
             sheet.write(row, column, result[row][column])
 
-    workbook.save("./run.xls")
+    workbook.save(os.path.split(os.path.realpath(__file__))[0]+"/run.xls")
--- evaluation/alpine/second_run_gear_images_without_cache.py
+++ evaluation/alpine/second_run_gear_images_without_cache.py
@@ -2,15 +2,13 @@ import sys
 # package need to be installed, pip install docker
 import docker 
 import time
-import random
 import yaml
 import os
+import random
 import subprocess
 import signal
 import urllib2
 import shutil
-import psycopg2
-import pymongo
 import xlwt
 # package need to be installed, apt-get install python-mysqldb
 import MySQLdb
@@ -25,7 +23,7 @@ apppath = ""
 # run paraments
 hostPort = 5000
 localVolume = ""
-pwd = os.getcwd()
+pwd = os.path.split(os.path.realpath(__file__))[0]
 
 runEnvironment = []
 runPorts = {}
@@ -35,7 +33,7 @@ runCommand = "echo hello"
 waitline = "hello"
 
 # result
-result = [["tag", "finishTime"], ]
+result = [["tag", "finishTime", "local data", "pull data"], ]
 
 class Runner:
 
@@ -87,7 +85,7 @@ class Runner:
                     elif container.logs().find(waitline) >= 0:
                         break
                     else:
-                        time.sleep(0.01)
+                        time.sleep(0.1)
                         pass
 
                 # print run time
@@ -95,9 +93,14 @@ class Runner:
 
                 print "finished in " , finishTime, "s"
 
-                data = get_net_data() - cnetdata
+                container_path = os.path.join("/var/lib/gear/private", private_repo)
+                local_data = subprocess.check_output(['du','-sh', container_path]).split()[0].decode('utf-8')
 
-                print "pull data: ", data
+                print "local data: ", local_data
+
+                pull_data = get_net_data() - cnetdata
+
+                print "pull data: ", pull_data
 
                 try: 
                     container.kill()
@@ -114,7 +117,7 @@ class Runner:
                 print "empty cache! \n"
 
                 # record the image and its Running time
-                result.append([tag, finishTime, data])
+                result.append([tag, finishTime, local_data, pull_data])
 
                 if auto != True: 
                     raw_input("Next?")
@@ -171,4 +174,4 @@ if __name__ == "__main__":
         for column in range(len(result[row])):
             sheet.write(row, column, result[row][column])
 
-    workbook.save("./second_run_without_cache.xls")
+    workbook.save(os.path.split(os.path.realpath(__file__))[0]+"/second_run_without_cache.xls")
